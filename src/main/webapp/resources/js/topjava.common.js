d760127e96f6b0c59a89c801d7e662ea4b0766ed
drovocek <mycodepg@gmail.com>
2020-12-19 18:18:52 +0300

    after patch 11_02

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -103,7 +103,7 @@ function failNoty(jqXHR) {
     console.log("detail: " + errorInfo.detail);
     failedNote = new Noty({
         text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;" + i18n["common.errorStatus"] + ": " + jqXHR.status +
-            "<br>" + errorInfo.type + "<br>" + errorInfo.detail,
+            "<br>" + errorInfo.type + "<br>" + errorInfo.details.join("<br>"),
         type: "error",
         layout: "bottomRight"
     }).show();
